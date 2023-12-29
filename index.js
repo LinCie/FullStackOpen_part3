987728dc9b56440f7f67bce318583df41cf264ee
LinCie <aldiofernanda@protonmail.com>
2023-12-29 16:37:59 +0700

    exercise 3.8 submission

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("reqbody", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :reqbody"
+  )
+);
 
 let numbers = [
   {
